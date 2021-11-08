da704ebbff49b283f625652e953da8626b222836
dallascaro <dallascaro98@gmail.com>
2021-11-08 11:03:20 -0600

    added pictures and background to home screen, developing first screens layout

--- App.js
+++ App.js
@@ -9,11 +9,18 @@ const Stack = createNativeStackNavigator();
 const homeScreen =  ({navigation}) => {
   return (
     <View style={styles.container}>
-      <Text>This is a homeScreen</Text>
+      <View>
+      <Image source = {require('./assets/strawberry/chocolateStrawBerry.png')}/>
+        <Image source = {require('./assets/strawberry/icons8-strawberry-64.png')}/> 
+      </View>
+      <Text style={styles.homePageTitle}>Fresh As Ny </Text>
+      <Text style={styles.homePageSubTitle}>Welcome to Fresh As Ny ! </Text>
+      <Image source = {require('./assets/strawberry/icons8-strawberry-64(1).png')}/>
+        <Image source = {require('./assets/strawberry/icons8-strawberry-64(1).png')}/>
       <StatusBar style="auto" />
       <Button
-      title = "Home Screen"
-      onPress = {() => navigation.navigate('Home')}>
+      title = "Search Screen"
+      onPress = {() => navigation.navigate('Search')}>
       </Button>
     </View>
   );
