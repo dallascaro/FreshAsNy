f84f2f3b4824dd94714d114a12e37b28dec12a87
dallascaro <dallas_caro@yahoo.com>
2021-11-07 19:42:07 -0600

    first Commit start of Project

--- App.js
+++ App.js
@@ -1,15 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const homeScreen =  ({navigation}) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is a homeScreen</Text>
       <StatusBar style="auto" />
+      <Button
+      title = "Home Screen"
+      onPress = {() => navigation.navigate('Home')}>
+      </Button>
     </View>
   );
+
 }
+
+const searchScreen =  ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text>This is a searchScreen</Text>
+      <StatusBar style="auto" />
+      <Button
+      title = "Home Screen"
+      onPress = {() => navigation.navigate('Home')}>
+      </Button>
+    </View>
+  );
+
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "Home">
+      <Stack.Screen name="Home" component={homeScreen} />
+      <Stack.Screen name="Search" component={searchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -17,5 +54,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#FFB6C1',
+    backgroundColor: '#FFB6C1'
   },
+
+  homePageTitle: {
+    color: '#FF6461',
+    fontFamily: 'Roboto',
+    fontSize: 48,
+  },
+  homePageSubTitle: {
+    color: '#FF6461',
+    fontFamily: 'Roboto',
+    fontSize: 18,
+  }
 });
