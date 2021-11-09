f92105663975924a27ecb7d1a7c1d24bef3e513f
dallascaro <dallascaro98@gmail.com>
2021-11-09 11:00:38 -0600

    Added emjoi character to page, working on fixing layout of all assests to complete home page

--- App.js
+++ App.js
@@ -9,19 +9,45 @@ const Stack = createNativeStackNavigator();
 const homeScreen =  ({navigation}) => {
   return (
     <View style={styles.container}>
-      <View>
-      <Image source = {require('./assets/strawberry/chocolateStrawBerry.png')}/>
-        <Image source = {require('./assets/strawberry/icons8-strawberry-64.png')}/> 
+
+    <View style = {styles.homePageStrawberry}>
+        <View style = {styles.homePageTopStrawberryRight}>
+          <Image source = {require('./assets/strawberry/chocolateStrawBerry.png')}/>
+        </View>
+
+        <View style = {styles.homePageTopStrawberryLeft}>
+          <Image source = {require('./assets/strawberry/icons8-strawberry-64.png')}/> 
+        </View>
+    </View>
+
+    <View style = {styles.homePageWords}>
+        <View style = {styles.homePageTitleView}>
+          <Text style={styles.homePageTitle}>Fresh As Ny </Text>
+          <Text style={styles.homePageSubTitle}>Welcome to Fresh As Ny ! </Text>
+        </View>
+    </View>
+
+    <View style = {styles.homePageEmojiView}>
+      <View style = {styles.homePageEmoji}>
+      <Image source = {require('./assets/babesEmoji.png')}/>
       </View>
-      <Text style={styles.homePageTitle}>Fresh As Ny </Text>
-      <Text style={styles.homePageSubTitle}>Welcome to Fresh As Ny ! </Text>
-      <Image source = {require('./assets/strawberry/icons8-strawberry-64(1).png')}/>
+    </View>
+
+    <View style = {styles.homePageBottomView}>
+      <View style = {styles.homePageBottomStrawberryLeft}>
         <Image source = {require('./assets/strawberry/icons8-strawberry-64(1).png')}/>
+      </View>
+     
+      <View style = {styles.homePageBottomStrawberryRight}>
+      <Image source = {require('./assets/strawberry/icons8-strawberry-64(1).png')}/>
+      </View>
+    </View>
+
       <StatusBar style="auto" />
-      <Button
-      title = "Search Screen"
-      onPress = {() => navigation.navigate('Search')}>
-      </Button>
+        <Button style = {styles.homePageButton}
+            title = "Search Screen"
+            onPress = {() => navigation.navigate('Search')}>
+        </Button>
     </View>
   );
 
@@ -64,7 +90,9 @@ const styles = StyleSheet.create({
     color: '#FFB6C1',
     backgroundColor: '#FFB6C1'
   },
-
+  homePageTitleView : {
+    paddingBottom: 50,
+  },
   homePageTitle: {
     color: '#FF6461',
     fontFamily: 'Roboto',
@@ -74,5 +102,41 @@ const styles = StyleSheet.create({
     color: '#FF6461',
     fontFamily: 'Roboto',
     fontSize: 18,
+  },
+  homePageStrawberry : {
+    flex: 1
+  },
+  homePageWords : {
+    flex: 1,
+  },
+  homePageEmojiView :{
+    flex: 2
+  },
+  homePageBottomView : {
+    flex: 4,
+  },
+  homePageTopStrawberryLeft :{
+    marginLeft: 270,
+    marginBottom: 10
+  },
+  homePageTopStrawberryRight :{
+    marginRight: 270,
+  },
+  homePageBottomStrawberryLeft :{
+    paddingRight: 10,
+    marginRight: 250,
+    marginTop: 175,
+    paddingTop: 50
+  },
+  homePageBottomStrawberryRight :{
+    marginLeft: 250,
+    paddingBottom: 20
+
+  },
+  homePageEmoji : {
+    marginRight: 100
+  },
+  homePageButton : {
+    paddingBottom: 10
   }
 });
